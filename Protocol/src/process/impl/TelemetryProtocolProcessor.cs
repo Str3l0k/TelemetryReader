198cc8f15fd5314dd7c295c8f09bef348e1fe2cd
Sebastian Barz <SebastianBarzDev@Gmail.com>
2018-12-10 14:10:59 +0100

    - removed recreated classes

--- Protocol/src/process/impl/TelemetryProtocolProcessor.cs
+++ Protocol/src/process/impl/TelemetryProtocolProcessor.cs
@@ -1,32 +1,19 @@
-﻿using Telemetry.Utilities;
-using System;
+﻿using System;
 using Telemetry.Read;
+using Telemetry.Utilities;
 
 namespace Telemetry.Process
 {
-    public delegate void DataProcessed<T>(T data);
-
     public class TelemetryProtocolProcessor<T> : IGameDataProcessor
     {
         // event after conversion
-        public event DataProcessed<T> OnDataProcessed;
-
-
         public Action<T> processedCallback;
         private T structure = Activator.CreateInstance<T>();
-        
-
-        //private Games.R3E.Data.Structure structure = new Games.R3E.Data.Structure();
 
         public void ProcessData(GameData data)
         {
-            //Debug.WriteLine("Processing data");
-
             var bytes = data.RawData;
             StructMarshal.MarshalRawDataToStruct(bytes, ref structure);
-
-            //Debug.WriteLine(structure.Gear);
-
             processedCallback?.Invoke(structure);
         }
     }
--- Reader UI/src/protocol/datapool/TelemetryValues.cs
+++ Reader UI/src/protocol/datapool/TelemetryValues.cs
@@ -1,50 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using TelemetryReader.src.protocol.datapool.categories;
+﻿using TelemetryReader.src.protocol.datapool.categories;
 
 namespace TelemetryReader.src.protocol
 {
     class TelemetryValues
     {
-        /* category accessor properties */
-        private Car car;
-        private Driver driver;
-        private Session session;
         private IDataWriter dataWriter;
 
-        internal Car Car
-        {
-            get
-            {
-                return car;
-            }
-        }
-
-        internal Driver Driver
-        {
-            get
-            {
-                return driver;
-            }
-        }
-
-        internal Session Session
-        {
-            get
-            {
-                return session;
-            }
-        }
+        internal Car Car { get; }
+        internal Driver Driver { get; }
+        internal Session Session { get; }
 
         TelemetryValues(IDataWriter dataWriter)
         {
             this.dataWriter = dataWriter;
 
-            car = new Car(dataWriter);
-            driver = new Driver(dataWriter);
-            session = new Session(dataWriter);
+            Car = new Car(dataWriter);
+            Driver = new Driver(dataWriter);
+            Session = new Session(dataWriter);
         }
     }
 }
